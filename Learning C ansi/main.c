fe0aa1e8f0866c3e6d2e8775bfbe4d6742fcf3e0
VitalyBesedin <vitaly_besedin@hotmail.com>
2019-02-05 21:22:19 +0300

    00004

--- Learning C ansi/main.c
+++ Learning C ansi/main.c
@@ -19,11 +19,11 @@ int main()
     lower = 0;              /* нижняя граница температур в таблице */
     upper = 300;            /* верхняя граница */
     step  = 20;             /* величина шага */
-    
+    printf("Фаренгейт - Цельсий\n");
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0) ;
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%6.0f %10.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
     
